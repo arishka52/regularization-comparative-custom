31c16f8c4ac8a0129d4ea3e8abc6169004dae2b8
arissshhhkkka <arishok2005@gmail.com>
2026-01-20 06:53:46 +0300

    Create experiments.py

---
+++ src/experiments.py
@@ -0,0 +1,9 @@
+"""
+Experiments:
+- run OLS, Ridge, Lasso, ElasticNet
+- grid search over lambdas / alphas
+- save metrics to reports/tables/results.csv
+"""
+
+def run_experiments_placeholder():
+    return "TODO: experiments will be implemented"
