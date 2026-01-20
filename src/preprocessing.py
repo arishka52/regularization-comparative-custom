5ac5a8b576ae499d62a4323ee9d509904f9f69fe
arissshhhkkka <arishok2005@gmail.com>
2026-01-20 06:52:22 +0300

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,11 @@
+"""
+Preprocessing:
+- load train.csv
+- handle missing values
+- one-hot encode categorical features
+- standardize features
+- train/test split
+"""
+
+def preprocess_placeholder():
+    return "TODO: preprocessing will be implemented"
